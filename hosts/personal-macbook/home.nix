a30e2e47ea77543bce8ba7577342848b7c799afd
NickMagic25 <majkic65@gmail.com>
2026-01-27 16:52:39 -0500

    lock file + home.nix fix

--- hosts/personal-macbook/home.nix
+++ hosts/personal-macbook/home.nix
@@ -3,6 +3,19 @@
 { config, pkgs, ... }:
 
 {
+  # Home Manager needs a bit of information about you and the paths it should manage
+  home.username = "nmajkic";
+  home.homeDirectory = "/Users/nmajkic";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
   # AeroSpace window manager configuration (shared across machines)
   xdg.configFile."aerospace/aerospace.toml".source = ../../shared/aerospace/aerospace.toml;
 }
