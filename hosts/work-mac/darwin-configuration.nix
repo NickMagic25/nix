c38d6d77c7f7a9965676903d09a4172827626699
NickMagic25 <majkic65@gmail.com>
2026-02-01 02:00:11 -0500

    btop and fastfetch on work mac

--- hosts/work-mac/darwin-configuration.nix
+++ hosts/work-mac/darwin-configuration.nix
@@ -18,6 +18,10 @@
       autoUpdate = false;
       cleanup = "none";  # Don't uninstall packages not in this config
     };
+    brews = [
+      "btop"
+      "fastfetch"
+    ]
   };
 
   # Add Homebrew to PATH (supports both Apple Silicon and Intel Macs)
