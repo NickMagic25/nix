8ec81dfd75ec4eeb71e003359b406e61fda37e92
Nick Majkic <majkic65@gmail.com>
2026-05-28 12:32:30 -0400

    watch sudo on personal laptop

--- hosts/personal-macbook/darwin-configuration.nix
+++ hosts/personal-macbook/darwin-configuration.nix
@@ -9,6 +9,9 @@
     ../../modules/darwin/homebrew.nix
   ];
 
+  # Enable Apple Watch for sudo (personal laptop only)
+  security.pam.services.sudo_local.watchIdAuth = true;
+
   # Disable nix-darwin's Nix management (using Determinate Systems Nix installer)
   nix.enable = false;
 
